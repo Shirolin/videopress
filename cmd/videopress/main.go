5ea6badf89f71584f777e980797bdfbd4406979e
Shirolin <shirolin.com@gmail.com>
2026-07-03 10:51:07 +0800

    feat(gui): 实现 CLI 与 GUI 的双入口合并，适配 wails.json 中的 maindir

--- cmd/videopress/main.go
+++ cmd/videopress/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"embed"
 	"fmt"
 	"os"
 	"path/filepath"
@@ -8,7 +9,14 @@ import (
 	"videopress/internal/app"
 	"videopress/internal/env"
 	"videopress/internal/sendto"
+
+	"github.com/wailsapp/wails/v2"
+	"github.com/wailsapp/wails/v2/pkg/options"
+	"github.com/wailsapp/wails/v2/pkg/options/assetserver"
 )
+
+//go:embed all:frontend/dist
+var assets embed.FS
 
 func main() {
 	executablePath, err := os.Executable()
@@ -16,15 +24,42 @@ func main() {
 		fmt.Fprintln(os.Stderr, "无法获取可执行文件路径:", err)
 		os.Exit(1)
 	}
-	exitCode := app.Execute(os.Args[1:], app.Dependencies{
-		ExecutableDir:   filepath.Dir(executablePath),
-		ExecutablePath:  executablePath,
-		Stdout:          os.Stdout,
-		Stderr:          os.Stderr,
-		InstallSendTo:   sendto.Install,
-		UninstallSendTo: sendto.Uninstall,
-		AddToPath:       env.AddToPath,
-		RemoveFromPath:  env.RemoveFromPath,
+	execDir := filepath.Dir(executablePath)
+
+	// 如果有命令行参数，走 CLI 模式
+	if len(os.Args) > 1 {
+		exitCode := app.Execute(os.Args[1:], app.Dependencies{
+			ExecutableDir:   execDir,
+			ExecutablePath:  executablePath,
+			Stdout:          os.Stdout,
+			Stderr:          os.Stderr,
+			InstallSendTo:   sendto.Install,
+			UninstallSendTo: sendto.Uninstall,
+			AddToPath:       env.AddToPath,
+			RemoveFromPath:  env.RemoveFromPath,
+		})
+		os.Exit(exitCode)
+	}
+
+	// 否则启动 GUI 模式 (Wails)
+	guiApp := NewApp(execDir, executablePath)
+
+	err = wails.Run(&options.App{
+		Title:  "Videopress",
+		Width:  850,
+		Height: 620,
+		AssetServer: &assetserver.Options{
+			Assets: assets,
+		},
+		BackgroundColour: &options.RGBA{R: 10, G: 10, B: 12, A: 1},
+		OnStartup:        guiApp.startup,
+		Bind: []interface{}{
+			guiApp,
+		},
 	})
-	os.Exit(exitCode)
+
+	if err != nil {
+		fmt.Fprintln(os.Stderr, "GUI 启动错误:", err)
+		os.Exit(1)
+	}
 }
